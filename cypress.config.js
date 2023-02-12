b02eb5852bbed9b3a853a61d3fbe9de76d4af2de
NibrazDSi <nibraz.khan@dsinnovators.com>
2023-02-13 00:51:30 +0600

    report added

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter:'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
       config.specPattern=[
         'cypress/e2e/Login.cy.js',
         'cypress/e2e/Sites/AddSites.cy.js',
